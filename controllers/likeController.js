bd641cc51f16d41b2113782a2a1b71a2b8a031e9
Igor <igorkuznecov@gmx.de>
2021-11-12 21:57:24 +0100

    blabla

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,34 +1,37 @@
-const Like = require('../models/Like')
+const Like = require("../models/Like");
 
 class LikeController {
     async create(req, res) {
-        const {userId, postId} = req.body
-        if (!userId || !postId) return res.json({message: "Missed userId or postId"})
-        const like = await Like.create({userId, postId})
-        res.json(like)
+        const { userId, postId } = req.body;
+        if (!userId || !postId)
+            return res.json({ message: "Missed userId or postId" });
+        const like = await Like.create({ userId, postId });
+        res.json(like);
     }
 
     async getAll(req, res) {
-        const likes = await Like.findAll()
-        return res.json(likes)
+        const likes = await Like.findAll();
+        return res.json(likes);
     }
 
     async getCount(req, res) {
-        const {postId} = req.params
-        const likes = Like.findAndCountAll({where: {postId}})
-        return res.json(likes)
+        const { postId } = req.params;
+        const likes = Like.findAndCountAll({ where: { postId } });
+        return res.json(likes);
     }
 
     async delete(req, res) {
         try {
-            const {userId} = req.body
-            if (!userId) return res.json({message: 'ID is not specified'})
-            await Like.destroy({where: {userId}})
-            return res.json({message: "Delete successfully"})
+            const { userId } = req.body;
+            console.log(req);
+            console.log(userId);
+            if (!userId) return res.json({ message: "ID is not specified" });
+            await Like.destroy({ where: { userId } });
+            return res.json({ message: "Delete successfully" });
         } catch (e) {
-            return res.json({message: e})
+            return res.json({ message: e });
         }
     }
 }
 
-module.exports = new LikeController()
+module.exports = new LikeController();
