a76d2da9e2a88e3bb56b0859ed8d34ce08b08c6e
Maksym Kostenko <kostenko.m.t@gmail.com>
2021-12-14 14:07:46 +0100

    commit

--- middleware/checkRoleAndTypeMiddleware.js
+++ middleware/checkRoleAndTypeMiddleware.js
@@ -4,7 +4,6 @@ module.exports = function (req, res, next) {
     try {
         const token = req.headers.authorization.split(' ')[1]
         const {type} = req.body
-        console.log(type)
         if (!token) return res.status(401).json({message: 'Not authorized'})
         const decoded = jwt.verify(token, process.env.SECRET_KEY)
         if (decoded.role !== 'ADMIN' && (type === 'VetHelp' || type === 'Hotels'))
